624dabf4314116b4bb7b29efdc3ee55dca6dcd23
unknown <xiongjoh@gmail.com>
2020-10-12 15:31:33 -0500

    linked to home

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default function SavedList(props) {
   return (
@@ -7,7 +8,7 @@ export default function SavedList(props) {
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <Link to={`/`} className="home-button">Home</Link>
     </div>
   );
 }
